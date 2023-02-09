3d59149ea7a548aab25ac370d91d877f566a8d66
vanshika <vanshika.arya@rapipay.com>
2023-02-09 13:13:54 +0530

    First Commit

---
+++ src/main/java/com/insurance/finaltest/FinalTestApplication.java
@@ -0,0 +1,14 @@
+package com.insurance.finaltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalTestApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Heyaaa");
+		SpringApplication.run(FinalTestApplication.class, args);
+	}
+
+}
